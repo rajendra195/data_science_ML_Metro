a4a9ccd8b4edb46871a0c77581c880c59de16370
rajendra195 <rajendraniroula12@gmail.com>
2024-03-05 10:19:54 +0545

    web scraping

--- week 3/day_02/Assignment/login_register.py
+++ week 3/day_02/Assignment/login_register.py
@@ -5,5 +5,7 @@ db_name = "user_data.db"
 conn = sqlite3.connect(db_name)
 cur = conn.cursor()
 
-def create_table(table_name):
-    if table_name
+# def create_table(table_name):
+#     if table_name.exists:
+#         print("Table already exists.")
+
