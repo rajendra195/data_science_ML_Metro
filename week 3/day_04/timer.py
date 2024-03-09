e91a1126c62d3344e0d851c05c085300f4a1c5d1
rajendra195 <rajendraniroula12@gmail.com>
2024-03-09 21:46:23 +0545

    some modification

--- week 3/day_04/timer.py
+++
@@ -1,8 +0,0 @@
-import timeit
-
-def timer(number, repeat):
-    def wrapper(func):
-        runs = timeit.repeat(func, number=number, repeat=repeat)
-        print(sum(runs) / len(runs))
-
-    return wrapper
--- week 3/day_04_05/asyncio_io.py
+++ week 3/day_04_05/asyncio_io.py
@@ -1,21 +1,44 @@
-import asyncio # asynchronous input output 
+# import asyncio # asynchronous input output 
+# import aiohttp
+# from program_timer import timer 
+# from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+
+# URL = "https://httpbin.org/uuid"
+
+# async def fetch_uuid(session, url):
+#     async with session.get(url) as response:
+#         json_data = await response.json()
+#         print(json_data['uuid'])
+
+
+# async def func():
+#     async with aiohttp.Clientsession() as session:
+#         tasks = [fetch_uuid(session, URL) for _ in range(100)]
+#         await asyncio.gather(*tasks)
+
+# @timer(1, 1)
+# async def main():
+#     asyncio.run(func())
+
+import asyncio
 import aiohttp
-from program_timer import timer 
-from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+
+
+from program_timer import timer
 
 URL = "https://httpbin.org/uuid"
 
-async def fetch_uuid(session, url):
+async def fetch(session, url):
     async with session.get(url) as response:
-        json_data = await response.json()
-        print(json_data['uuid'])
+        json_response = await response.json()
+        print(json_response["uuid"])
 
-
-async def func():
-    async with aiohttp.Clientsession() as session:
-        tasks = [fetch_uuid(session, URL) for _ in range(100)]
+async def main():
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch(session, URL) for _ in range(100)]
         await asyncio.gather(*tasks)
 
 @timer(1, 1)
-async def main():
-    asyncio.run(func())
+def func():
+    asyncio.run(main())
+
