fc379ba8926f7c6648b81cdf66859d56a974ae74
rajendra195 <rajendraniroula12@gmail.com>
2024-03-13 00:03:19 +0545

    visualization practice

--- week_04/stats/Practice/class_stat.py
+++ week_04/stats/Practice/class_stat.py
@@ -1,26 +1,25 @@
+import numpy as np
 class Statictics:
     def mean(self, data):
+        total_sum = sum(data)
+        no = len(data)
+        return total_sum / no
         
-        for item in data:
-            sums = sum(item)
-            no_of_items = len(item)
-        mean = sums / no_of_items
-        return mean
-    
-    def median(self, data):
+    def quartiles(self, datas, quartile):
+        data = sorted(datas)
+        length = len(data)
         
-        for item in data:
-            n = len(item)
-
-        if n % 2 != 0:
-            median_item = (n + 1) / 2
-            median = data[int(median_item) - 1]
-            return median
+        if length % 2 != 0:
+            q = data[((quartile * (length + 1)) // 4) - 1]
         else:
-            median_item = (n + 1) // 2
-            median = (data[median_item - 1] + data[median_item]) / 2
-            return median
+            th_item = quartile * (length + 1) // 4
+            q = (data[th_item - 1] + data[th_item]) / 2
+        return q
 
 stats = Statictics()
-median = stats.median(3, 2, 5, 2, 2, 1, 1, 3)
+median = stats.quartiles([3, 4, 2, 5, 2, 5, 2, 5, 2], 3)
 print(median)
+print(np.percentile([3, 4, 2, 5, 2, 5, 2, 5, 2], 3))
+
+
+print(stats.mean([3, 3, 5, 2, 4, 2]))
